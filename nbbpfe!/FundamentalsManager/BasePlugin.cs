abee5ecef17da36a41b9f81af4b441befd2cf3a3
Nyan_Dev <enzowroldbr@gmail.com>
2024-09-20 17:16:52 -0300

    Adicionar arquivos de projeto.

---
+++ nbbpfe!/FundamentalsManager/BasePlugin.cs
@@ -0,0 +1,52 @@
+﻿using BepInEx;
+using HarmonyLib;
+using MTM101BaldAPI;
+using MTM101BaldAPI.AssetTools;
+using MTM101BaldAPI.Registers;
+using MTM101BaldAPI.SaveSystem;
+using System.Collections;
+using UnityEngine;
+
+namespace nbbpfe.FundamentalsManager
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("mtm101.rulerp.bbplus.baldidevapi", MTM101BaldiDevAPI.VersionNumber)]
+    [BepInProcess("BALDI.exe")]
+    public class BasePlugin : BaseUnityPlugin
+    {
+        public static BasePlugin Instance { get; private set; }
+
+        private void Awake()
+        {
+            Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            Instance = this;
+            harmony.PatchAllConditionals();
+
+            LoadingEvents.RegisterOnAssetsLoaded(Info, PreLoad(), false);
+            //LoadingEvents.RegisterOnAssetsLoaded(Info, PostLoad(), true);
+
+            ModdedSaveGame.AddSaveHandler(Info);
+        }
+
+        private IEnumerator PreLoad()
+        {
+            yield return 1;
+            yield return "Lodaing...";
+        }
+
+        /*
+        IEnumerator PostLoad()
+        {
+            yield return 1;
+            yield return "Postload message";
+        }
+        */
+    }
+
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "whiytellydeveloper.plugin.mod.newbaldibasicspluseducationalfundamentals";
+        public const string PLUGIN_NAME = "New Baldi's Basics Plus Educational Fundamentals";
+        public const string PLUGIN_VERSION = "1.0";
+    }
+}
---
+++ nbbpfe!/FundamentalsManager/FundamentalLoaderManager.cs
@@ -0,0 +1,12 @@
+﻿
+namespace nbbpfe.FundamentalsManager
+{
+    public static class FundamentalLoaderManager
+    {
+        public class FloorData(string floor = "None")
+        {
+            public string Floor => _floor;
+            readonly string _floor = floor;
+        }
+    }
+}
